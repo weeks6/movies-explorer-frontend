8f0ee3504294ac889a9c7119f48ca2bfcc822b52
weeks6 <bridcarp1@gmail.com>
2021-08-02 20:16:52 +0300

    файловая структура  и шрифты

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  return (<div className='page'>
+
+
+  </div>);
+}
+
+export default App;
